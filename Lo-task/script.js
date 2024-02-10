1232e3239a30b715c924d278cf19e7f6124cc710
J. Júnior <135084173+jjuniorbrasil@users.noreply.github.com>
2024-02-10 18:47:25 -0300

    Lo-task - Lista de tarefas
    Uma pequena lita de tarefas que fiz pra dar utilidade à música que compûs.

---
+++ Lo-task/script.js
@@ -0,0 +1,10 @@
+var qtdtask = 0;
+var taskbox = document.querySelector(".taskbox");
+
+function adicionarTarefa() {
+    taskbox.style.display = "block";
+}
+
+function taskAdd() {
+    taskbox.style.display = "none";
+}
