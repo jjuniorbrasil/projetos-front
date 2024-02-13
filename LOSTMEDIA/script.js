41e3c5af63b54c99aa740b3305c9dbd4c161300f
J. Júnior <135084173+jjuniorbrasil@users.noreply.github.com>
2024-02-13 10:22:07 -0300

    Finalização: Cores, elementos visuais e DOM básico.

--- LOSTMEDIA/script.js
+++ LOSTMEDIA/script.js
@@ -2,3 +2,20 @@ function getSubmitData() {
     let form = document.getElementById('mediasubmit');
     form.classList.add('translate');
 }
+
+function submitAnswer() {
+    let username = document.getElementById('username');
+    let mediatitle = document.getElementById('mediatitle');
+    let mediareason = document.getElementById('mediareason');
+    window.alert("Data I should use for something \n\nuser: " + username.value + '\nmedia: ' + mediatitle.value + "\nreason: " + mediareason.value);
+    username.value = "";
+    mediatitle.value = "";
+    mediareason.value = "";
+    let form = document.getElementById('mediasubmit');
+    form.classList.remove('translate');
+}
+
+function showHead() {
+    let head = document.getElementById('headgradient');
+    head.classList.add('translate');
+}
